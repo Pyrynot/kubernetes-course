a829ee379d2b5f3ceec8a18deffd5656e95aafd6
Pyry Heinoja <pyryheinoja1@kamk.fi>
2024-08-09 19:29:42 +0300

    Update env

--- part3/3.03/app/pg-backup.sh
+++ part3/3.03/app/pg-backup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 POSTGRES_POD_NAME=$(kubectl get pods --selector=app=your-postgres-app -o jsonpath='{.items[0].metadata.name}')
-POSTGRES_USER="your_postgres_user"
-DATABASE_NAME="your_database_name"
+POSTGRES_USER="postgres"
+POSTGRES_HOST="postgres-svc"
+POSTGRES_PORT="5432"
+DATABASE_NAME="postgres"
 BACKUP_FILE="/backup/db_backup_$(date +%Y%m%d%H%M).sql"
 
 if [ -z "$POSTGRES_POD_NAME" ]; then
@@ -10,7 +12,7 @@ if [ -z "$POSTGRES_POD_NAME" ]; then
   exit 1
 fi
 
-kubectl exec $POSTGRES_POD_NAME -- bash -c "pg_dump -U $POSTGRES_USER $DATABASE_NAME" > $BACKUP_FILE
+kubectl exec $POSTGRES_POD_NAME -- bash -c "PGPASSWORD=$POSTGRES_PASSWORD pg_dump -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER $DATABASE_NAME" > $BACKUP_FILE
 
 if [ $? -ne 0 ]; then
   echo "Error: Failed to create database backup."
